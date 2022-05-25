ade7160a43199233e723e2a7dbd61b83d82ae40a
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-25 12:58:22 -0400

    update

---
+++ uncertainties/src/components/Header.svelte
@@ -0,0 +1,29 @@
+<script></script>
+
+<header>
+  <h1>
+    VISUALIZING<br /> UNCERTAINTY
+  </h1>
+  <h3>Uncertainty as A Result of Selection of Different Analytical Methods</h3>
+</header>
+
+<style>
+  header {
+    height: 30%;
+    padding-top: 2vh;
+    padding-bottom: 2vh;
+    border-bottom: 1px #fff solid;
+  }
+
+  h1 {
+    font-family: "Syncopate";
+    color: #800020;
+    font-weight: 700;
+    padding-bottom: 0;
+    /* font-family: "Goldman"; */
+    font-size: 3rem;
+    /* background: #fff; */
+    margin: 0 auto;
+    text-align: center;
+  }
+</style>
